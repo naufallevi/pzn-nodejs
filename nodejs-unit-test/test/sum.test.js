52e7b34c735e8dba94b842bab7b0a60b1aab9d3a
Muhammad Naufal Pahlevi <naufal260902@gmail.com>
2026-08-12 13:00:44 +0700

    (unit-test) matchers

---
+++ nodejs-unit-test/test/sum.test.js
@@ -0,0 +1,16 @@
+import { sum } from "../src/sum";
+
+test("test sum function 1", () => {
+  const result = sum(5, 5);
+  expect(result).toBe(10);
+});
+
+test("test sum function 2", () => {
+  const result = sum(5, 5);
+  expect(result).toBe(10);
+});
+
+test("test sum function 3", () => {
+  const result = sum(5, 5);
+  expect(result).toBe(10);
+});
